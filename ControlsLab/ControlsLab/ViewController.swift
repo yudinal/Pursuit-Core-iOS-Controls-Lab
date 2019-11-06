1872b2dae77259b7a152553c9811d84879d1d5e9
Ben Stone <benjamin.h.stone@gmail.com>
2019-11-06 12:10:33 -0500

    add ibaction implementation

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -2,22 +2,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: -IBOutlets
+    
     @IBOutlet var valueLabels: [UILabel]!
     @IBOutlet var suitImageViews: [UIImageView]!
     @IBOutlet var cardView: UIView!
     
+    // MARK: -Internal variables
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    var card: Card = Card(suit: .heart, value: .ace)
+    
+    // MARK: -IBActions
     
     @IBAction func newSuitSelected(_ sender: UISegmentedControl) {
-        guard let suit = Suit(rawValue: sender.selectedSegmentIndex) else {
+        guard let newSuit = Suit(rawValue: sender.selectedSegmentIndex) else {
             fatalError("Developer Error: Unknown suit")
         }
+        card = Card(suit: newSuit, value: card.value)
     }
     
     @IBAction func cardValueChanged(_ sender: UIStepper) {
+        guard let newValue = CardValue(rawValue: Int(sender.value)) else {
+            fatalError("Developer Error: Invalid card value")
+        }
+        card = Card(suit: card.suit, value: newValue)
     }
 }
 

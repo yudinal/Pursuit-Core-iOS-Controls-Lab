ac7f8280a24d36f4828545ce3f7cd4766ceb08df
Ben Stone <benjamin.h.stone@gmail.com>
2019-11-06 11:27:28 -0500

    add outlets and actions

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -2,8 +2,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var valueLabels: [UILabel]!
+    @IBOutlet var suitImageViews: [UIImageView]!
+    @IBOutlet var cardView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func newSuitSelected(_ sender: UISegmentedControl) {
+    }
+    @IBAction func cardValueChanged(_ sender: UIStepper) {
     }
 }
 
